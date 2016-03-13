196b36870fe097b2783a26657f01a987234cfce5
vovanovak <2411vova@gmail.com>
2016-03-13 20:43:41 +0200

    template method add_element to array and enter_string to set the value of string

--- OpLab7/OpLab7/main.cpp
+++ OpLab7/OpLab7/main.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+int length = 0; //Довжина покажчиків на масиви characters і length
+char * characters = new char[length]; //Покажчик на масив унікальних символів(містить всі символи рядка str без повторів)
+int * numbers = new int[length];//Покажчик на масив кількостей символів в рядку str
+char * str = new char[255]; // Покажчик на масив символів str
+
+//Шаблонна підпрограма додавання нового елементу elem в масив arr, довжиною length 
+template<typename T>
+T * add_element(T * arr, T elem, int& length){
+	T * res = new T[length + 1]; 
+	for (int i = 0; i < length; i++){
+		res[i] = arr[i];
+	}
+	res[length] = elem;
+	delete[] arr;
+	return res;
+}
+//Підпрограма вводу рядку str
+void enter_string(char * str){
+	cout << "Enter str: ";
+	cin.getline(str, 255);
+}
+
+
 void main(){
-	
+		
 }
